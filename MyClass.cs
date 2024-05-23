4f14bb336e68b475f199925538461e6dd9dc4d2e
ThePaulper <pnboris2020@gmail.com>
2024-05-22 23:24:59 -0600

    Fixed Bugs, established variables

---
+++ MyClass.cs
@@ -0,0 +1,18 @@
+public class ClassExample
+{
+    
+}
+
+public class ClassWithVariables
+{
+    public int number = 2;
+    public string userName = "Bob";
+}
+
+public class ClassWithFunctions
+{
+    public void RunFunction()
+    {
+        Console.WriteLine("Running Function");
+    }
+}   
